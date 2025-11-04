dcac152070a13a0611cb7c80a6c344f984a35677
Ricardo Ramirez <ricardoramirezcaero123@gmail.com>
2025-11-04 02:33:45 -0400

    feat: add 'calendar-events' route

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,6 +3,7 @@ import healthRouter from './health';
 import authRouter from '../modules/auth/auth.routes';
 import verificationsRouter from '../modules/verifications/verification.routes';
 import categoriesRouter from '../modules/categories/categories.routes';
+import calendarEventsRouter from '../modules/calendar-events/calendar-event.routes';
 
 const router = Router();
 
@@ -10,5 +11,6 @@ router.use('/health', healthRouter);
 router.use('/auth', authRouter);
 router.use('/verifications', verificationsRouter);
 router.use('/categories', categoriesRouter);
+router.use('/calendar-events', calendarEventsRouter);
 
 export default router;
