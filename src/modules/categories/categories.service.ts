eaebce11e7f21a5929565809c74d43ab970846f4
Diego Perez <diego.fernando.perez.luna@gmail.com>
2025-11-10 19:41:15 -0400

    Add: categories destination for menu

--- src/modules/categories/categories.service.ts
+++ src/modules/categories/categories.service.ts
@@ -3,11 +3,13 @@ import { Db } from 'mongodb';
 interface Category {
   name: string;
   description?: string;
+  destination?: string;
 }
 
 export async function getCategoriesService(db: Db): Promise<Category[]> {
   const categories = await db.collection<Category>('categories').find().toArray();
   return categories.map(category => ({
+    destination: category.destination,
     name: category.name,
     description: category.description,
   }));
